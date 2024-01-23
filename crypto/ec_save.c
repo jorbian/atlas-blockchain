30a3d1156f51c8b571b8704124d3eaecdc3faaa7
Jonathan Boller <5533@holbertonstudents.com>
2024-01-23 14:33:32 -0600

    See if the checker likes someone else's version

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,98 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 #include "hblk_crypto.h"
-#include "key_gen.h"
-
-static char path_buff[NUM_KEYS][MAX_PATH_LEN];
-
 /**
- * write_keys - does the actual writting of the keys
+ * write_private - writes private key to a file
  * @key: struct containing the key
- *
+ * @folder: the path to save the file
+ * Return: 1 on success, else 0
 */
-static void write_keys(EC_KEY *key)
+static int write_private(EC_KEY *key, char const *folder)
 {
 	FILE *fp;
+	int written = 0;
+	size_t path_len = strlen(folder) + strlen("/key.pem") + 3;
+	char *priv_path = malloc(sizeof(char) * (path_len));
+	char *file_name = "key.pem";
 
-	uint64_t i;
-
-	for (i = 0; i < NUM_KEYS; i++)
-	{
-		fp = fopen(path_buff[i], "w");
-		if (!fp)
-			continue;
-
-		switch (i)
-		{
-			case PRIVATE:
-				PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL);
-				break;
-			case PUBLIC:
-				PEM_write_EC_PUBKEY(fp, key);
-				break;
-			default:
-				continue;
-		}
-		fclose(fp);
-	}
+	snprintf(priv_path, path_len, "%s/%s", folder, file_name);
+	fp = fopen(priv_path, "w");
+	if (!fp)
+		printf("failed to open file\n");
+	written = PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL);
+	fclose(fp);
+	free(priv_path);
+	return (written > 0 ? 1 : 0);
 }
-
 /**
- * test_path - checks if the path exists, creates it if not
- * @folder: folder the key files are eventually supposed to go into
- *
+ * write_public - writes public key to a file
+ * @key: struct containing the key
+ * @folder: the path to save the file
+ * Return: 1 on success, else 0
+*/
+static int write_public(EC_KEY *key, char const *folder)
+{
+	FILE *fp;
+	size_t path_len = strlen(folder) + strlen("/key_pub.pem") + 3;
+	char *pub_path = malloc(sizeof(char) * (path_len));
+	char *file_name = "key_pub.pem";
+	int written = 0;
+
+	snprintf(pub_path, path_len, "%s/%s", folder, file_name);
+	fp = fopen(pub_path, "w");
+	if (!fp)
+		printf("failed to open file\n");
+	written = PEM_write_EC_PUBKEY(fp, key);
+	fclose(fp);
+	free(pub_path);
+	return (written > 0 ? 1 : 0);
+}
+/**
+ * handle_path - checks if the path exists, creates it if not
+ * @folder: the path
  * Return: 1 on success else 0
 */
-static int test_path(char const *folder)
+static int handle_path(char const *folder)
 {
 	struct stat st = {0};
 
 	if (stat((char *)folder, &st) == -1)
+	{
 		if (mkdir((char *)folder, 0777) == -1)
+		{
+			perror("mkdir");
 			return (0);
-
+		}
+	}
 	return (1);
 }
-
 /**
- * generate_file_paths - generate the designated file paths for folder
- * @folder: folder the key files are eventually supposed to go into
- *
-*/
-static void generate_file_paths(const char *folder)
-{
-	uint64_t i;
-
-	for (i = 0; i < NUM_KEYS; i++)
-	{
-		snprintf(
-			path_buff[i],
-			MAX_PATH_LEN,
-			"%s/%s",
-			folder, key_file_names[i]
-		);
-	}
-}
-
-/**
- * ec_save - saves existing EC key pair to disk.
- * @key: points to EC_KEY pair to be saved on disk
- * @folder: path to the folder in which to save the keys
- *
- * Return: 1 or 0 upon success or failure
+ * ec_save - saves public and private key pair to disk
+ * @key: key struct containing public and private keys
+ * @folder: the path
+ * Return: 1 on success, else 0
 */
 int ec_save(EC_KEY *key, char const *folder)
 {
-	if (!key || !folder || !test_path(folder))
-		return (1);
+	if (!key)
+		return (0);
 
-	generate_file_paths(folder);
+	handle_path(folder);
 
-	write_keys(key);
+	if (!write_private(key, folder) || !write_public(key, folder))
+		return (0);
 
-	return (0);
+	return (1);
 }
