0a3f36b30f28968c17b440b8d699c4b6433ce2cb
Jonathan Boller <5533@holbertonstudents.com>
2024-02-16 16:56:25 -0600

    And here we go

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,6 +1,36 @@
 #include "blockchain.h"
 
 /**
+ * count_leading_zeros - count the number of leading zeros in provided hash
+ * @array: pointer to the hashed data
+ * @size: how big the data is
+ *
+ * Return: How many leading zeros there were.
+*/
+static uint32_t count_leading_zeros(uint8_t const *array, uint32_t size)
+{
+	uint32_t leading_zeros = 0;
+
+	int32_t i, j, value;
+
+	for (i = 0; i < size; i++)
+	{
+		value = array[i];
+
+		for (j = 7; j >= 0; j--)
+		{
+			if ((value >> j) & 1)
+				break;
+
+			leading_zeros++;
+		}
+		if ((value >> j) & 1)
+			break;
+	}
+	return (leading_zeros);
+}
+
+/**
  * hash_matches_difficulty - checks if hash matches a given difficulty
  * @hash: hash to check
  * @difficulty: minimum difficulty the hash should match
@@ -10,8 +40,8 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 	uint32_t difficulty)
 {
-	(void)hash;
-	(void)difficulty;
+	if (count_leading_zeros(hash, SHA256_DIGEST_LENGTH) >= difficulty)
+		return (1);
 
 	return (0);
 }
