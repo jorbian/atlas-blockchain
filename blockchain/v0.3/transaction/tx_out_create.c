89442f9bf5f6e35bb6ca559d8cbc619260b602a9
Jonathan Boller <5533@holbertonstudents.com>
2024-02-28 00:30:43 -0600

    E isso que e isso

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -7,11 +7,22 @@
  *
  * Return: pointer to transaction output on success, or NULL on failure
 */
-tx_out_t *tx_out_create(uint32_t amount,
-	uint8_t const pub[EC_PUB_LEN])
+tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
-	(void)amount;
-	(void)pub;
+	tx_out_t *tx_change = NULL;
 
-	return (NULL);
+	tx_change = calloc(1, sizeof(tx_out_t));
+	if (!tx_change)
+		return (NULL);
+
+	tx_change->amount = amount;
+
+	memcpy(tx_change->pub, pub, EC_PUB_LEN);
+
+	SHA256(
+		(const unsigned char *)tx_change,
+		sizeof(tx_change->amount) + EC_PUB_LEN,
+		tx_change->hash
+	);
+	return (tx_change);
 }
