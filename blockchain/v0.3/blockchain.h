bbcd6267a201765625c0d8516bbfc9daa1c7cd10
Jonathan Boller <5533@holbertonstudents.com>
2024-03-01 18:25:25 -0600

    And now?

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -110,7 +110,8 @@ uint8_t *block_hash(
 	block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
-int block_is_valid(block_t const *block, block_t const *prev_block);
+int block_is_valid(block_t const *block, block_t const *prev_block,
+	llist_t *all_unspent);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty);
 void block_mine(block_t *block);
 uint32_t blockchain_difficulty(blockchain_t const *blockchain);
