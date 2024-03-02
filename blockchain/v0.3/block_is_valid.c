1dfecbd5aa57d8aa66abe508a2432fa5646e456d
Jonathan Boller <5533@holbertonstudents.com>
2024-03-01 18:22:44 -0600

    Seeing whatever works.

--- blockchain/v0.3/block_is_valid.c
+++ blockchain/v0.3/block_is_valid.c
@@ -4,13 +4,16 @@
  * block_is_valid - verifies that a Block is valid
  * @block: points to the Block to check
  * @prev_block: points to previous Block in the chain (or NULL)
+ * @all_unspent: unspent blocks
  *
  * Return: A description goes here.
 */
-int block_is_valid(block_t const *block, block_t const *prev_block)
+int block_is_valid(block_t const *block, block_t const *prev_block,
+	llist_t *all_unspent)
 {
 	(void)block;
 	(void)prev_block;
+	(void)all_unspent;
 
 	return (-1);
 }
