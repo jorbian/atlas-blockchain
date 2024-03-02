88f8b2fa3e4d69dc26e0f56a6e64240c6b9d986a
Jonathan Boller <5533@holbertonstudents.com>
2024-03-01 18:39:04 -0600

    Quick test

--- blockchain/v0.3/transaction/transaction_hash.c
+++ blockchain/v0.3/transaction/transaction_hash.c
@@ -11,8 +11,46 @@ uint8_t *transaction_hash(
 	transaction_t const *transaction,
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	(void)transaction;
-	(void)hash_buf;
+	uint8_t *buf_to_hash;
+	uint32_t buf_to_hash_len, buffer_offset = 0, input_qty, output_qty;
+	uint32_t inputs_len, outputs_len, index;
+	tx_in_t *this_input;
+	tx_out_t *this_output;
 
-	return (NULL);
-}
+	if (!transaction || !hash_buf)
+		return (NULL);
+
+	input_qty = llist_size(transaction->inputs);
+	output_qty = llist_size(transaction->outputs);
+
+	inputs_len = input_qty * TX_IN_HASH_QTY * SHA256_DIGEST_LENGTH;
+	outputs_len = output_qty * SHA256_DIGEST_LENGTH;
+
+	buf_to_hash_len = inputs_len + outputs_len;
+
+	buf_to_hash = malloc(buf_to_hash_len);
+	if (!buf_to_hash)
+		return (NULL);
+
+	for (index = 0; index < input_qty; index++)
+	{
+		this_input = llist_get_node_at(transaction->inputs, index);
+		memcpy(buf_to_hash + buffer_offset,
+			this_input,
+			TX_IN_HASH_QTY * SHA256_DIGEST_LENGTH);
+		buffer_offset += TX_IN_HASH_QTY * SHA256_DIGEST_LENGTH;
+	}
+	for (index = 0; index < output_qty; index++)
+	{
+		this_output = llist_get_node_at(transaction->outputs, index);
+		memcpy(buf_to_hash + buffer_offset,
+			this_output->hash,
+			SHA256_DIGEST_LENGTH);
+		buffer_offset += SHA256_DIGEST_LENGTH;
+	}
+
+	SHA256(buf_to_hash, buf_to_hash_len, hash_buf);
+	free(buf_to_hash);
+
+	return (hash_buf);
+
