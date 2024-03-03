57a2dee43c821455129b8c552353c84a6bc7fec4
Jonathan Boller <5533@holbertonstudents.com>
2024-03-03 00:26:16 -0600

    just quickly testing something

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -14,34 +14,29 @@
 block_t *block_create(
 	block_t const *prev, int8_t const *data, uint32_t data_len)
 {
-	block_t *new_block;
+	block_t *block;
 
-	new_block = calloc(1, sizeof(block_t));
-	if (!new_block)
+	block = malloc(sizeof(block_t));
+	if (!block)
 		return (NULL);
-
 	if (data_len > BLOCKCHAIN_DATA_MAX)
 		data_len = BLOCKCHAIN_DATA_MAX;
-
-	new_block->info.difficulty = 0;
-	new_block->info.nonce = 0;
-	new_block->info.index = 0;
-
-	time((time_t *)&new_block->info.timestamp);
-
-	memset(new_block->hash, 0, SHA256_DIGEST_LENGTH);
-	memset(new_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-
+	block->info.difficulty = 0;
+	time((time_t *)&block->info.timestamp);
+	block->info.nonce = 0;
 	if (prev)
 	{
-		new_block->info.index = prev->info.index + 1;
-		memset(new_block->data.buffer, 0, sizeof(new_block->data.buffer));
-		memcpy(new_block->info.prev_hash, prev->hash, sizeof(prev->hash));
+		block->info.index = prev->info.index + 1;
+		memset(block->data.buffer, 0, sizeof(block->data.buffer));
+		memcpy(block->info.prev_hash, prev->hash, sizeof(prev->hash));
 	}
-	memcpy(new_block->data.buffer, data, data_len);
-
-	new_block->data.len = data_len;
-	new_block->transactions = llist_create(MT_SUPPORT_FALSE);
-
-	return (new_block);
+	else /* genesis block is created in blockchain_create, impossible */
+	{
+		block->info.index = 0;
+		memset(block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+	}
+	memset(block->hash, 0, SHA256_DIGEST_LENGTH);
+	memcpy(block->data.buffer, data, data_len), block->data.len = data_len;
+	block->transactions = llist_create(MT_SUPPORT_FALSE); /* transaction */
+	return (block);
 }
