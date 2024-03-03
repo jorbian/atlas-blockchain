396f535b5c261f0cc8295c3281ae52c94109c110
Jonathan Boller <5533@holbertonstudents.com>
2024-03-03 00:13:30 -0600

    Don't know why this wasn't the first thing I did...

--- blockchain/v0.3/transaction/transaction_destroy.c
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -7,5 +7,14 @@
 */
 void transaction_destroy(transaction_t *transaction)
 {
-	(void)transaction;
+	if (transaction)
+	{
+		if (transaction->inputs)
+			llist_destroy(transaction->inputs, 1, NULL);
+
+		if (transaction->outputs)
+			llist_destroy(transaction->outputs, 1, NULL);
+
+		free(transaction);
+	}
 }
