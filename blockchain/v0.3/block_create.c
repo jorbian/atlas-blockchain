e3ba53a144bef3a05d516ec86c65f6b50bb1c47b
Jonathan Boller <5533@holbertonstudents.com>
2024-03-03 00:27:37 -0600

    See if this fixes the bsic problem

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -8,35 +8,40 @@
  * @prev: The address of the previous block in the blockchain
  * @data: The place in memory to duplicate to store in the block's data
  * @data_len: The size in bytes of the data to duplicate and store
- *
+ * 
  * Return: The address of the newly created block
 */
 block_t *block_create(
 	block_t const *prev, int8_t const *data, uint32_t data_len)
 {
-	block_t *block;
+	block_t *new_block;
 
-	block = malloc(sizeof(block_t));
-	if (!block)
+	new_block = calloc(1, sizeof(block_t));
+	if (!new_block)
 		return (NULL);
+
 	if (data_len > BLOCKCHAIN_DATA_MAX)
 		data_len = BLOCKCHAIN_DATA_MAX;
-	block->info.difficulty = 0;
-	time((time_t *)&block->info.timestamp);
-	block->info.nonce = 0;
+
+	new_block->info.difficulty = 0;
+	new_block->info.nonce = 0;
+	new_block->info.index = 0;
+
+	time((time_t *)&new_block->info.timestamp);
+
+	memset(new_block->hash, 0, SHA256_DIGEST_LENGTH);
+	memset(new_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+
 	if (prev)
 	{
-		block->info.index = prev->info.index + 1;
-		memset(block->data.buffer, 0, sizeof(block->data.buffer));
-		memcpy(block->info.prev_hash, prev->hash, sizeof(prev->hash));
+		new_block->info.index = prev->info.index + 1;
+		memset(new_block->data.buffer, 0, sizeof(new_block->data.buffer));
+		memcpy(new_block->info.prev_hash, prev->hash, sizeof(prev->hash));
 	}
-	else /* genesis block is created in blockchain_create, impossible */
-	{
-		block->info.index = 0;
-		memset(block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-	}
-	memset(block->hash, 0, SHA256_DIGEST_LENGTH);
-	memcpy(block->data.buffer, data, data_len), block->data.len = data_len;
-	block->transactions = llist_create(MT_SUPPORT_FALSE); /* transaction */
-	return (block);
+	memcpy(new_block->data.buffer, data, data_len);
+
+	new_block->data.len = data_len;
+	new_block->transactions = llist_create(MT_SUPPORT_FALSE);
+
+	return (new_block);
 }
--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -7,6 +7,12 @@
 */
 void block_destroy(block_t *block)
 {
-	if (block)
-		free(block);
+	if (!block)
+		return;
+
+	llist_destroy(
+		block->transactions, 1,
+		(node_dtor_t)transaction_destroy
+	);
+	free(block);
 }
